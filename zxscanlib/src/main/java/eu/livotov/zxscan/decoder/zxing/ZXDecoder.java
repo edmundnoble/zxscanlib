88e6363a39a4f5d0c8c8679324d44bd6a51adea3
Edmund Noble <eanoble@uwaterloo.ca>
2015-04-23 23:26:20 -0400

    Fixed compile error.

--- zxscanlib/src/main/java/eu/livotov/zxscan/decoder/zxing/ZXDecoder.java
+++ zxscanlib/src/main/java/eu/livotov/zxscan/decoder/zxing/ZXDecoder.java
@@ -26,6 +26,8 @@ public class ZXDecoder implements BarcodeDecoder
 
     public final static Collection<BarcodeFormat> QR_CODE_FORMATS = EnumSet.of(BarcodeFormat.QR_CODE);
     public final static Collection<BarcodeFormat> DATA_MATRIX_FORMATS = EnumSet.of(BarcodeFormat.DATA_MATRIX);
+    public final static Collection<BarcodeFormat> ALL_FORMATS = EnumSet.of(BarcodeFormat.CODE_39, BarcodeFormat.CODE_93, BarcodeFormat.CODE_128, BarcodeFormat.ITF, BarcodeFormat.CODABAR, BarcodeFormat.UPC_A, BarcodeFormat.UPC_E, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.RSS_14, BarcodeFormat.RSS_EXPANDED, BarcodeFormat.QR_CODE, BarcodeFormat.DATA_MATRIX);
+
     protected Map<DecodeHintType, Object> hints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
     private MultiFormatReader reader;
 
@@ -42,12 +44,7 @@ public class ZXDecoder implements BarcodeDecoder
 
     public ZXDecoder()
     {
-        Collection<BarcodeFormat> decodeFormats = EnumSet.noneOf(BarcodeFormat.class);
-        decodeFormats.addAll(QR_CODE_FORMATS);
-        decodeFormats.addAll(ONE_D_FORMATS);
-        decodeFormats.addAll(DATA_MATRIX_FORMATS);
-
-	this(decodeFormats);
+	this(ALL_FORMATS);
     }
 
     public String decode(final byte[] image, final int width, final int height)
